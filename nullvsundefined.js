d44be8b0397a697cb2b7d49126f0aad83e9110f9
Dalwer Hossain <dalwer.official@gmail.com>
2021-02-02 16:33:32 +0600

    add all

---
+++ nullvsundefined.js
@@ -0,0 +1,27 @@
+//undefined----- it happened when u call the value of that has not exist inside the function object;
+
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return
+// }
+
+// function add(num1,num2)
+// {
+//     console.log(num1,num2);
+// }
+
+
+// const result = {name:"smart dube", phone:"02132464"};
+// console.log(result.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,3,5];
+console.log(ages[6]);
+
+// null---- it occurs when we set the null explicitly. 
+
+let a = null;
+
+console.log(a);
